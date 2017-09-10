ab74a1157af0a25d329c500f626705b56c7d7e14
Philip Wisner <philipwisner@gmail.com>
2017-09-10 12:34:02 +0200

    fixed errors

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,42 +3,41 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
-    this.direction = "";
+    this.direction = "UP";
     this.timerId;
     this.waitingList = []; //made request but not on yet
     this.passengers = []; //currently in elevator
   }
 
   start() {
-    this.timerId = setInterval(update, 1000);
-  }
+    this.timer = setInterval(() => { this.update() }, 1000);
 
   stop() {
-    this.timerId = clearInterval(update, 0);
+    clearInterval(this.timer);;
   }
 
   update() {
-    log();
+    this.log();
   }
 
   _passengersEnter() {
-    for (var i = 0; i < waitingList.length; i++) {
+    this.waitingList.forEach((person, index) => {
       if (this.waitingList[i] = this.floor) {
         this.passengers.push(this.waitingList[i]);
         this.waitingList.splice(this.waitingList[i], 1);
         requests.push(Person.destinationFloor);
         return console.log(`${Person.name} has entered the elevator`);
       };
-    }
+    })
   }
 
   _passengersLeave() {
-    for (var i = 0; i < passengers.length; i++) {
+    this.passengers.forEach((passengers, index) => {
       if (this.passengers[i].destinationFloor = this.floor) {
         this.passengers.splice(this.passengers[i], 1);
         return console.log(`${Person.name} has left the elevator`);
       };
-    }
+    })
   }
 
   floorUp() {
@@ -56,7 +55,7 @@ class Elevator {
   }
 
   call() {
-    var newPerson = new Person();
+    let newPerson = new Person();
     this.requests.push(newPerson.originFloor);
     this.waitingList.push(newPerson);
     //ADD WHOLE person object and add it as a request to the elevators queue - request array
